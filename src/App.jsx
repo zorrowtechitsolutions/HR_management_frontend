1153556d58cb0cef922114f66f9963990cf52afe
Shanayasmin123 <shanayasmin219@gmail.com>
2026-03-24 09:45:07 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { Button } from './components/ui/button'
 
 export default function App() {
   return (
-    <div>
-      <Button>Hello</Button>
-    </div>
+    <div></div>
   )
 }
